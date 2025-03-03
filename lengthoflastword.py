2fc5357771dcb386ce62b7cb5808902b91b3cff8
deonflx <deonnjr10@gmail.com>
2025-03-03 15:17:28 +0530

    slit mod

--- lengthoflastword.py
+++ lengthoflastword.py
@@ -1,5 +1,18 @@
-def lengthOfLastWord(self, s):
-       last=s.split()
-       print(len(last[-1]))
-       return len(last[-1])
-lengthOfLastWord("","my name is deon")
+class Solution:
+    def lengthOfLastWord(self, s):
+       
+        words = s.strip().split()
+        
+
+        if not words:
+            return 0
+            
+        
+        return len(words[-1])
+
+
+    
+        
+        test_str = "my name is deon"
+        result = solution.lengthOfLastWord(test_str)
+        
