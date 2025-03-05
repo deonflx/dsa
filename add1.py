1dea03306f341d88d2fb9af68334768274462ac1
deonflx <deonnjr10@gmail.com>
2025-03-05 21:49:32 +0530

    valid palindrome

--- add1.py
+++ add1.py
@@ -1,11 +1,36 @@
 class Solution(object):
     def plusOne(self, digits):
-        if digits[-1]==9:
-           digits.pop()
-           digits.append(1)
-           digits.append(0)
-        else:
-            digits[-1]=(digits[-1]+1)   
+        
+        if not digits:
+            return [1]
+            
+        
+        digits = list(digits)
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+        
+       
+        return [1] + digits
 
-        return digits
-    print(plusOne(" ",[9]) )   
+
+    # Create instance
+    solution = Solution()
+    
+    # Test cases
+    test_cases = [
+        [9, 9],
+        [1, 2, 3],
+        [9],
+        [9, 9, 9],
+        [1,9,9,9]
+    ]
+    
+    # Run tests
+    for test in test_cases:
+        result = solution.plusOne(test)
+        print(f"Input: {test} -> Output: {result}")
+
