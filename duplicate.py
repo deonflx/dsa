62858ad81a79ff29a159bfad64ed0db5eb1c6714
deonflx <deonnjr10@gmail.com>
2025-03-07 21:52:08 +0530

    rev,

---
+++ duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        for i in nums:
+            if(nums.count(i)>1):
+                return True
+            
+        return False        
+        
